58003c00e65c0d2c08df137ffa19e2d87e8588d6
Alex Bisaillion <a.bisaillion@gmail.com>
2019-07-01 18:54:18 -0400

    Add feed to express router

--- server/routes/stats.js
+++ server/routes/stats.js
@@ -4,30 +4,43 @@ var statGenerator = require('../spotify/statGenerator');
 
 /* GET stats listing. */
 router.get('/', function(req, res, next) {
-    if (req.query.timeRange && req.query.numResults) {
-        if(req.query.type === "tracks") {
-            statGenerator.getTopTracks(req.query.timeRange, req.query.numResults).then(
-                function (data) {
-                    console.log("Completed successfully.");
-                    res.json(data);
-                },
-                function (err) {
-                    console.log("Something went wrong!", err);
-                }
-            )
+  if(req.query.type === "tracks") {
+    if (req.query.numResults) {
+      statGenerator.getTopTracks(req.query.timeRange, req.query.numResults).then(
+        function (data) {
+          console.log("Completed successfully.");
+          res.json(data);
+        },
+        function (err) {
+          console.log("Something went wrong!", err);
         }
-        else if (req.query.type === "artists") {
-            statGenerator.getTopArtists(req.query.timeRange, req.query.numResults).then(
-                function (data) {
-                    console.log("Completed successfully.");
-                    res.json(data);
-                },
-                function (err) {
-                    console.log("Something went wrong!", err);
-                }
-            )
-        }
+      )
     }
+  } else if (req.query.type === "artists") {
+    if (req.query.numResults) {
+      statGenerator.getTopArtists(req.query.timeRange, req.query.numResults).then(
+        function (data) {
+          console.log("Completed successfully.");
+          res.json(data);
+        },
+        function (err) {
+          console.log("Something went wrong!", err);
+        }
+      )
+    }
+  } else if (req.query.type === "type") {
+    if (req.query.numResults) {
+      statGenerator.getFeed(req.query.numResults).then(
+        function (data) {
+          console.log("Completed successfully.");
+          res.json(data);
+        },
+        function (err) {
+          console.log("Something went wrong!", err);
+        }
+      )
+    }
+  }
 });
 
 module.exports = router;
