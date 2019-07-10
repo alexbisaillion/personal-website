87d62482f7231bacfbd47fa426f5d7d8ac38fc17
Alex Bisaillion <a.bisaillion@gmail.com>
2019-07-10 19:57:30 -0400

    Add a gradient to list entries

--- client/src/components/ListEntry.js
+++ client/src/components/ListEntry.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
 import moment from 'moment';
+import * as Vibrant from 'node-vibrant'
 import './ListEntry.css'
 
 const seenLive = ["The Weeknd", "6LACK", "Belly", "Rae Sremmurd", "Kendrick Lamar", "DRAM", "YG", "ScHoolboy Q", "Jay Rock", "Ab-Soul", "SiR", "Lance Skiiiwalker", "Travis Scott", "Sheck Wes"];
 
 class ListEntry extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {dominantColour: "black" };
+  }
+
+  async componentDidMount() {
+    await Vibrant.from(this.props.art).getPalette().then((palette) => this.setState({ dominantColour: palette.Vibrant.hex }));
+  }
+
   render() {
+    //Vibrant.from(this.props.art).getPalette().then((palette) => this.setState({ dominantColour: palette.Vibrant.hex }));
+
     let formattedArtist;
     if (seenLive.includes(this.props.artist)) {
       formattedArtist = <span className="list-entry-artist-live">{this.props.artist}</span>;
@@ -13,8 +25,10 @@ class ListEntry extends Component {
       formattedArtist = <span className="list-entry-artist">{this.props.artist}</span>;
     }
 
+    let gradient = `linear-gradient(to right, ${this.state.dominantColour}, black, black, black, black)`;
+
     return (
-      <div className="list-entry-container">
+      <div className="list-entry-container" style={{background: gradient}}>
         <img src={this.props.art} alt="art" className="list-entry-image"/>
         <div className="list-entry-info-container">
           {formattedArtist}
