782818cf5ae6f70a13d107cbdb1874444f90d958
Alex Bisaillion <a.bisaillion@gmail.com>
2019-07-13 22:10:20 -0400

    Make the sound wave animation smart

--- client/src/components/CurrentTrack.js
+++ client/src/components/CurrentTrack.js
@@ -6,24 +6,25 @@ import Soundwave from './Soundwave';
 class CurrentTrack extends Component {
   constructor(props) {
     super(props);
-    this.state = {currentTrack: {}, dominantColour: "black" };
+    this.state = {currentTrack: {}, dominantColour: "black", infoWidth: 0 };
+    this.info = React.createRef();
   }
 
   async componentDidMount() {
     const currentTrack = await fetch(`/stats?type=currentTrack`);
     const currentTrackJSON = await currentTrack.json();
     const colour = await Vibrant.from(currentTrackJSON.art).getPalette();
-    this.setState({ currentTrack: currentTrackJSON, dominantColour: colour.Vibrant.hex});
+    this.setState({ currentTrack: currentTrackJSON, dominantColour: colour.Vibrant.hex, infoWidth: this.info.current.offsetWidth});
   }
 
   render() {
     let gradient = `linear-gradient(to right, black, black, ${this.state.dominantColour})`;
     return (
       <div className="current-track-container" style={{background: gradient}}>
-        <div className="current-track-info-container">
+        <div className="current-track-info-container" ref={this.info}>
           <div className="current-track-artist">{this.state.currentTrack.artist}</div>
           <div className="current-track-title">{this.state.currentTrack.track}</div>
-          <Soundwave colour={this.state.dominantColour}></Soundwave>
+          <Soundwave colour={this.state.dominantColour} infoWidth={this.state.infoWidth * 0.5}></Soundwave>
         </div>
         <img src={this.state.currentTrack.art} alt="art" className="current-track-image"/>
       </div>
--- client/src/components/Soundwave.js
+++ client/src/components/Soundwave.js
@@ -3,21 +3,23 @@ import './Soundwave.css'
 
 class Soundwave extends Component {
   render() {
+    let bars = [];
+    let numBars = Math.floor(this.props.infoWidth / 35);
+    for (let i = 0; i < numBars; i++) {
+      bars.push(<div className='bar' style={{background: this.props.colour, animationDuration: `${getRandomInt(600, 700)}ms`}}></div>);
+    }
     return (
       <div id='bars'>
-        <div className='bar' style={{background: this.props.colour}}></div>
-        <div className='bar' style={{background: this.props.colour}}></div>
-        <div className='bar' style={{background: this.props.colour}}></div>
-        <div className='bar' style={{background: this.props.colour}}></div>
-        <div className='bar' style={{background: this.props.colour}}></div>
-        <div className='bar' style={{background: this.props.colour}}></div>
-        <div className='bar' style={{background: this.props.colour}}></div>
-        <div className='bar' style={{background: this.props.colour}}></div>
-        <div className='bar' style={{background: this.props.colour}}></div>
-        <div className='bar' style={{background: this.props.colour}}></div>
+        {bars}
       </div>
     );
   }
 }
-  
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
 export default Soundwave;
