6acf6bc441e2bc155072f51abb4b7d8a2d21edbb
Alex Bisaillion <a.bisaillion@gmail.com>
2019-10-27 17:18:54 -0400

    Update styling on homepage introduction and current track

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import LoadingIcon from './LoadingIcon';
 import Soundwave from './Soundwave';
 import HomeCard from './HomeCard';
+
 import './Home.css'
 const homeCards = require('../data/homeCards');
 
@@ -27,11 +28,11 @@ class Home extends Component {
     let currentlyPlaying;
 
     if (this.state.isLoading) {
-      currentlyPlaying = <div className="currentlyPlaying" ref={this.info} style={{minHeight: "260px"}}><LoadingIcon height="100px" width="100px"></LoadingIcon></div>
+      currentlyPlaying = <div className="currentlyPlaying" ref={this.info} style={{minHeight: "260px", width: "90%"}}><LoadingIcon height="100px" width="100px"></LoadingIcon></div>
     } else {
       let gradient = `radial-gradient(circle at top, #181818, #181818, ${this.state.currentTrack.colour})`
       currentlyPlaying = (
-        <div className="currentlyPlaying" style={{background: gradient, height: "260px"}} ref={this.info}>
+        <div className="currentlyPlaying" style={{background: gradient, height: "260px", width: "90%", borderRadius: "25px", borderStyle: "solid", borderColor: this.state.currentTrack.colour}} ref={this.info}>
           <span style={{ fontSize: "1em", marginRight: "30px"}}>{this.state.currentTrack.isPlaying ? "I'm currently listening to..." : "I last listened to..."}</span>
           <img src={this.state.currentTrack.art} alt="art" className="trackArt"/>
           <div className="trackInformation">
@@ -44,8 +45,11 @@ class Home extends Component {
     }
     return (
       <div className="home-container">
-        <span className="sectionHeader">Alex Bisaillion</span>
-        <p>Hi there! I'm Alex Bisaillion, a third-year computer science student at Carleton University. This is my personal website, where I experiment with web development. Enjoy your stay!</p>
+        <div className="introduction">
+          <span className="sectionHeader">ALEX BISAILLION</span>
+          <p>Hi there! I'm Alex Bisaillion, a third-year computer science student at Carleton University. This is my personal website, where I experiment with web development. Enjoy your stay!</p>
+        </div>
+
         <div className="cards">
           <HomeCard info={homeCards[0]}></HomeCard>
           <HomeCard info={homeCards[1]}></HomeCard>
