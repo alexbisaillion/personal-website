4ab13a7b7cabc3fd4d8b6a9b83e3ed29e5911fc0
Alex Bisaillion <a.bisaillion@gmail.com>
2019-10-31 12:28:41 -0400

    Update Side Project page styling

--- client/src/components/SideProject.js
+++ client/src/components/SideProject.js
@@ -1,25 +1,82 @@
 import React, { Component } from 'react';
+import ImageZoom from 'react-medium-image-zoom'
 import './SideProject.css';
+import { ReactComponent as GitHub } from '../img/github.svg';
 
 class SideProject extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      name: this.props.name,
+      description: this.props.description,
+      technologies: this.props.technologies,
+      github: this.props.github,
+      images: this.props.images,
+      hoverItem: ""
+    };
+  }
+
   render() {
+    console.log(this.state);
     return (
       <div className="side-project-container">
         <div className="side-project-header">
-          <span>{this.props.name}</span>
-          <div className="side-project-icons">
-            {this.props.technologies.map(item =>
-              <item.image className="technology-image" height="60px" width="60px" style={{fill: "white", title: item.name}} title={item.name}></item.image>
+          <span>{this.state.name}</span>
+        </div>
+        <div className="side-project-body">
+
+          <div className="side-project-info">
+            {this.state.description}
+            <br></br>
+
+            <div className="side-project-icons">
+              {this.state.technologies.map(item =>
+                <item.image 
+                  onMouseEnter = {() => this.setState({hoverItem: item.name})}
+                  onMouseLeave = {() => this.setState({hoverItem: ""})}
+                  height = "60px"
+                  width = "60px"
+                  style = {{fill: this.state.hoverItem === item.name ? item.colour : "white", title: item.name}}
+                  title = {item.name}
+                  key = {item.name}
+                >
+                </item.image>
+              )}
+            </div>
+            <a href={this.state.github} target="_blank" rel="noopener noreferrer">
+              <div className="side-project-github">
+                  <span>VIEW ON GITHUB</span>
+                  <GitHub width={50} height={50}></GitHub>
+              </div>
+            </a>
+          </div>
+
+          <div className="side-project-image-grid">
+            {this.state.images.map(item =>
+              <ImageZoom
+                image={{
+                  src: item,
+                  alt: 'sideproject',
+                  className: 'img',
+                  style: { width: '100%' }
+                }}
+                zoomImage={{
+                  src: item,
+                  alt: 'sideproject'
+                }}
+                defaultStyles={{
+                  overlay: {
+                    backgroundColor: 'none'
+                  }
+                }}
+              key={item}
+              />
             )}
           </div>
+
+          {/*<span style={{alignSelf: "center"}}>View on <a href={this.props.github}>GitHub</a></span>*/}
+
         </div>
-        <span>{this.props.description}</span>
-        <div className="side-project-image-grid">
-          {this.props.images.map(item =>
-            <img src={item} width="100%" alt="sideproject"/>
-          )}
-        </div>
-        <span style={{alignSelf: "center"}}>View on <a href={this.props.github}>GitHub</a></span>
       </div>
     )
   }
