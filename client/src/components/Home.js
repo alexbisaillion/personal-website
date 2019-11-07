e7e4af320e4e3209df7b59399f47099e0ac7ee60
Alex Bisaillion <a.bisaillion@gmail.com>
2019-11-06 21:52:38 -0500

    Make current track on home page mobile friendly

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -28,18 +28,18 @@ class Home extends Component {
     let currentlyPlaying;
 
     if (this.state.isLoading) {
-      currentlyPlaying = <div className="currentlyPlaying" ref={this.info} style={{minHeight: "260px", width: "90%"}}><LoadingIcon height="100px" width="100px"></LoadingIcon></div>
+      currentlyPlaying = <div className="currentlyPlaying" ref={this.info}><LoadingIcon height="100px" width="100px"></LoadingIcon></div>
     } else {
       let gradient = `radial-gradient(circle at top, #181818, #181818, ${this.state.currentTrack.colour})`
       currentlyPlaying = (
-        <div className="currentlyPlaying" style={{background: gradient, height: "260px", width: "90%", borderRadius: "25px", borderStyle: "solid", borderColor: this.state.currentTrack.colour}} ref={this.info}>
-          <span style={{ fontSize: "1vw", marginRight: "30px"}}>{this.state.currentTrack.isPlaying ? "I'm currently listening to..." : "I last listened to..."}</span>
+        <div className="currentlyPlaying" style={{background: gradient, borderRadius: "25px", borderStyle: "solid", borderColor: this.state.currentTrack.colour}} ref={this.info}>
+          <span style={{ fontSize: "1em" }}>{this.state.currentTrack.isPlaying ? "I'm currently listening to..." : "I last listened to..."}</span>
           <img src={this.state.currentTrack.art} alt="art" className="trackArt"/>
           <div className="trackInformation">
             <span className="trackArtist">{this.state.currentTrack.artist}</span>
-            <span style={{ fontSize: "1.5vw"}}><i>{this.state.currentTrack.track}</i></span>
+            <span style={{ fontSize: "1.25em"}}><i>{this.state.currentTrack.track}</i></span>
           </div>
-          <Soundwave colour={this.state.currentTrack.colour} infoWidth={this.state.infoWidth * 0.15} isPlaying={this.state.currentTrack.isPlaying} height="40%"></Soundwave>
+          <Soundwave colour={this.state.currentTrack.colour} infoWidth={this.state.infoWidth * 0.15} isPlaying={this.state.currentTrack.isPlaying} height="100px"></Soundwave>
         </div>
       )
     }
